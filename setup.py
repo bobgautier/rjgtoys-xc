1e6ade2fccfafa4ed0f54a821d8d1c73c07abf86
Bob Gautier <bob@rjg-resources.com>
2020-02-05 21:40:21 +0000

    Add URL

--- setup.py
+++ setup.py
@@ -8,10 +8,14 @@ setup(
     version = "0.0.1",
     author = "Bob Gautier",
     author_email = "bob.gautier@gmail.com",
+    url = "https://github.com/bobgautier/rjgtoys-xc",
     description = ("Structured exceptions"),
     namespace_packages=['rjgtoys'],
     packages = ['rjgtoys','rjgtoys.xc'],
     install_requires = [
         'pydantic',
-    ]
+    ],
+    extras_require = {
+        'autodoc': ['sphinx_autodoc_typehints']
+    }
 )
